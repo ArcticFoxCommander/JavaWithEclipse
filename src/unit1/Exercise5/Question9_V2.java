731207630e01d8c4c39f1f5da09d802bfdf1869e
ArticFoxCommander <60055478+ArticFoxCommander@users.noreply.github.com>
2022-09-28 12:46:54 -0700

    Correct Indentation for Question 9 Version 2

--- src/unit1/Exercise5/Question9_V2.java
+++ src/unit1/Exercise5/Question9_V2.java
@@ -8,26 +8,26 @@ public class Question9_V2 {
 		// TODO Auto-generated method stub
 
 		Scanner input = new Scanner(System.in);
-		
+
 		System.out.print("Input 3 numbers: ");
 		double a = input.nextDouble();
 		double b = input.nextDouble();
 		double c = input.nextDouble();
-		
+
 		double d = (b * b) - (4 * a * c);
-	
+
 		if(d < 0) {
 			System.out.println("The equation has no real roots !!!");
 		}
 		else {
 			if(d == 0) {
 				System.out.println("The equation has one root !!!" + (-1 * b)/(2 * a));
-		}
-		else {
-			double r1 = ((-1 * b) + Math.pow(d, 0.5)) / (2 * a);
-			double r2 = ((-1 * b) - Math.pow(d, 0.5)) / (2 * a);
-			System.out.printf("The equation has two roots %.2f and %.2f", r1, r2);
-		
+			}
+			else {
+				double r1 = ((-1 * b) + Math.pow(d, 0.5)) / (2 * a);
+				double r2 = ((-1 * b) - Math.pow(d, 0.5)) / (2 * a);
+				System.out.printf("The equation has two roots %.2f and %.2f", r1, r2);
+
 				input.close();
 			}
 		}
