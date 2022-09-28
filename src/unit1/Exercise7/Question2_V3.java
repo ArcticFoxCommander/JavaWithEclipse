84d08666663e8433de4d12d9a07ffac86c3eaa89
ArticFoxCommander <60055478+ArticFoxCommander@users.noreply.github.com>
2022-09-28 12:29:53 -0700

    Version 3 of Question 2

---
+++ src/unit1/Exercise7/Question2_V3.java
@@ -0,0 +1,18 @@
+package unit1.Exercise7;
+
+public class Question2_V3 {
+
+	public static void main(String[] args) {
+		
+		int x = 3, y = 3, z = 0;
+		
+		if (x > 2) 
+			if (y > 2) {
+				z = x + y;
+				System.out.println("z is " + z);
+			
+		}
+		else 
+			System.out.println("x is " + x);
+	}
+}
