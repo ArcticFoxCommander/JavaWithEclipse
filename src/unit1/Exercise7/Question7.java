473de657559ec9b7e85f10d5d3e946dbca916386
ArticFoxCommander <60055478+ArticFoxCommander@users.noreply.github.com>
2022-09-28 13:17:45 -0700

    Create Question7.java
    Number is ...

---
+++ src/unit1/Exercise7/Question7.java
@@ -0,0 +1,15 @@
+package unit1.Exercise7;
+
+public class Question7 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int number = 15;
+		
+		if (number % 2 == 0)
+			System.out.println(number + " is even");
+		else if (number % 5 == 0)
+			System.out.println(number + " is a multiple of 5");
+	}
+}
