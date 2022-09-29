8d318e29c8a72b2f7bcb85075775da134837c516
ArticFoxCommander <60055478+ArticFoxCommander@users.noreply.github.com>
2022-09-29 14:43:26 -0700

    Create Question3.java
    Write a Boolean expression for |x – 5| < 4.5

---
+++ src/unit1/Exercise8/Question3.java
@@ -0,0 +1,13 @@
+package unit1.Exercise8;
+
+public class Question3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		double x = 2;
+		System.out.println(
+			(x - 5) < 4.5 && (x-5 > - 4.5));
+		
+	}
+
+}
