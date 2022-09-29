9eb05c1f8ab8d6071057f6ec7eec79d934737bdf
ArticFoxCommander <60055478+ArticFoxCommander@users.noreply.github.com>
2022-09-29 14:35:42 -0700

    Create Question2 + Version 2
    A & B
    
    && and ||

---
+++ src/unit1/Exercise8/Question2.java
@@ -0,0 +1,15 @@
+package unit1.Exercise8;
+
+public class Question2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		double num = 50;
+		
+		System.out.println(
+		(num > 1) && (num < 100));
+		
+		
+	}
+
+}
---
+++ src/unit1/Exercise8/Question2_V2.java
@@ -0,0 +1,15 @@
+package unit1.Exercise8;
+
+public class Question2_V2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		double num = 50;
+		
+		System.out.println(
+		(num < 100) || (num < -1));
+		
+		
+	}
+
+}
