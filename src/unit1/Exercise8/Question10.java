40110f9ac6e21cdb0954a39ac39341832b9016e4
ArcticFoxCommander <60055478+ArticFoxCommander@users.noreply.github.com>
2022-10-03 10:11:27 -0700

    Weight Checker
    Question 9 and 10

---
+++ src/unit1/Exercise8/Question10.java
@@ -0,0 +1,14 @@
+package unit1.Exercise8;
+
+public class Question10 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int pounds = 56;
+		int inches = 70;
+		
+		System.out.println(
+		(pounds > 50 && inches > 60));
+	}
+
+}
---
+++ src/unit1/Exercise8/Question9.java
@@ -0,0 +1,14 @@
+package unit1.Exercise8;
+
+public class Question9 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int pounds = 48;
+		int inches = 70;
+		
+		System.out.println(
+		(pounds > 50 || inches > 60));
+	}
+
+}
