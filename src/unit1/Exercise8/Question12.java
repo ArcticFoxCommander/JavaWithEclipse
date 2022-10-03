66713cac00497f630866ef07ca408e32b9a587a7
ArcticFoxCommander <60055478+ArticFoxCommander@users.noreply.github.com>
2022-10-03 09:29:43 -0700

    Create Question12.java

---
+++ src/unit1/Exercise8/Question12.java
@@ -0,0 +1,25 @@
+package unit1.Exercise8;
+
+import java.util.Scanner;
+
+public class Question12 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner input = new Scanner(System.in);
+				
+		System.out.println("Enter a three digit number: ");
+		int number = input.nextInt();
+		
+		int first = number / 100;
+		int last = number % 100 % 10;
+		
+		if (first == last) {
+		System.out.println("The number " + number + " is a palindrome.");}
+		else {
+			System.out.println("The number " + number + " is not a palindrome.");}
+		
+		input.close();
+	}
+
+}
