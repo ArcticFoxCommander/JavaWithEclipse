dee414a07b34a3e7c002ee3d9cf7d05cb229a94f
ArcticFoxCommander <60055478+ArticFoxCommander@users.noreply.github.com>
2022-10-03 09:30:04 -0700

    Create Question8.java

---
+++ src/unit1/Exercise8/Question8.java
@@ -0,0 +1,13 @@
+package unit1.Exercise8;
+
+public class Question8 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int age = 15;
+		
+		System.out.println(
+		(age > 13 && age < 18));
+	}
+
+}
