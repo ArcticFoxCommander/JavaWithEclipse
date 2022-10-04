69888b0582b78b3259eff40c255c1ff39a6797ba
ArcticFoxCommander <60055478+ArticFoxCommander@users.noreply.github.com>
2022-10-04 11:34:34 -0700

    Switch Case (Q1-2)

---
+++ src/unit1/Exercise9/Question1.java
@@ -0,0 +1,15 @@
+package unit1.Exercise9;
+
+public class Question1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x = 3, y = 3;
+		
+		if (x + 3 == 6)
+			y = 1;
+		
+		else y += 1;
+		
+		}
+	}
---
+++ src/unit1/Exercise9/Question2.java
@@ -0,0 +1,20 @@
+package unit1.Exercise9;
+
+public class Question2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x = 1, a = 3;
+		
+		switch (a) {
+		
+			case 1: x += 5; break;
+			case 2: x += 10; break;
+			case 3: x += 16; break;
+			case 4: x += 34;
+			
+		}
+		
+	}
+
+}
