168cb2d42fc003402dda06ed522c2f4fa0de2a71
ArcticFoxCommander <60055478+ArticFoxCommander@users.noreply.github.com>
2022-10-05 12:21:07 -0700

    Create Question3.java

---
+++ src/unit1/Exercise9/Question3.java
@@ -0,0 +1,23 @@
+package unit1.Exercise9;
+
+public class Question3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int day = 4;
+
+		switch (day) {
+
+		case 0: System.out.println("Sunday"); break;
+		case 1: System.out.println("Monday"); break;
+		case 2: System.out.println("Tuesday"); break;
+		case 3: System.out.println("Wednesday"); break;
+		case 4: System.out.println("Thursday"); break;
+		case 5: System.out.println("Friday"); break;
+		case 6: System.out.println("Sunday"); break;
+
+
+		}
+	}
+}
