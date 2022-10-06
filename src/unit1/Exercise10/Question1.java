1647d463a406814898287c9502629bcfef9ec2cc
ArcticFoxCommander <60055478+ArticFoxCommander@users.noreply.github.com>
2022-10-06 14:17:02 -0700

    Create Question1.java

---
+++ src/unit1/Exercise10/Question1.java
@@ -0,0 +1,26 @@
+package unit1.Exercise10;
+
+public class Question1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.printf("%d\n", (int) '1');
+		System.out.printf("%d\n", (int) 'A');
+		System.out.printf("%d\n", (int) 'B');
+		System.out.printf("%d\n", (int) 'a');
+		System.out.printf("%d\n", (int) 'b');
+		
+		System.out.println((char)40);
+		System.out.println((char)59);
+		System.out.println((char)79);
+		System.out.println((char)85);
+		System.out.println((char)90);
+		
+		System.out.println((char)0X40);
+		System.out.println((char)0X5A);
+		System.out.println((char)0X71);
+		System.out.println((char)0X72);
+		System.out.println((char)0X7A);
+	}
+
+}
