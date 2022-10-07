8c1b06dbea5133960e669a5d8466f7812ae6376d
ArcticFoxCommander <60055478+ArticFoxCommander@users.noreply.github.com>
2022-10-07 08:30:39 -0700

    Create Question3.java

---
+++ src/unit1/Exercise10/Question3.java
@@ -0,0 +1,11 @@
+package unit1.Exercise10;
+
+public class Question3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("\\");
+		System.out.println("\"");
+	}
+
+}
