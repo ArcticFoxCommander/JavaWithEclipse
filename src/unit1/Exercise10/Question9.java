193d50e262fa83fc6f6f47f2c9783909d16e4216
ArcticFoxCommander <60055478+ArticFoxCommander@users.noreply.github.com>
2022-10-07 09:59:08 -0700

    Create Question9.java

---
+++ src/unit1/Exercise10/Question9.java
@@ -0,0 +1,17 @@
+package unit1.Exercise10;
+
+public class Question9 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		java.util.Scanner input = new java.util.Scanner(System.in);
+		System.out.print("Input an ASCII code: ");
+		int dec = input.nextInt();
+		
+		System.out.println((char)dec);
+		
+		input.close();
+	}
+
+}
