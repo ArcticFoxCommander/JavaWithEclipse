0c8957e790469f3fa8bbeb050571c6617de087ea
ArcticFoxCommander <60055478+ArticFoxCommander@users.noreply.github.com>
2022-10-07 08:54:47 -0700

    Create Question8.java

---
+++ src/unit1/Exercise10/Question8.java
@@ -0,0 +1,15 @@
+package unit1.Exercise10;
+
+public class Question8 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println('a' < 'b');
+		System.out.println('a' <= 'A');
+		System.out.println('a' > 'b');
+		System.out.println('a' >= 'A');
+		System.out.println('a' == 'a');
+		System.out.println('a' != 'b');
+	}
+
+}
