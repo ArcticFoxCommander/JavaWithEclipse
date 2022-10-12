a2c9459047c5344ebaf64c40ef903fac96de808a
ArcticFoxCommander <60055478+ArticFoxCommander@users.noreply.github.com>
2022-10-12 13:29:37 -0700

    Create Question1.java

---
+++ src/unit1/Exercise11/Question1.java
@@ -0,0 +1,38 @@
+package unit1.Exercise11;
+
+public class Question1 {
+
+	public static void main(String[] args) {
+		// Strings
+		String s1 = "Welcome to Java";
+		String s2 = "Programming is fun";
+		String s3 = "Welcome to Java";
+		
+		if (s1 == s2) 
+			System.out.println("True");
+		else
+			System.out.println("False");
+		
+		if (s2 == s3)
+			System.out.println("True");
+		else
+			System.out.println("False");
+		
+		if (s1.equals(s2))
+			System.out.println("True");
+		else
+			System.out.println("False");
+		
+		if (s1.equals(s3))
+			System.out.println("True");
+		else
+			System.out.println("False");
+		
+		
+		
+		
+		
+		
+		
+	}
+}
