77d05f91ee528b23919775549a59f3db3df5fff7
ArcticFoxCommander <60055478+ArticFoxCommander@users.noreply.github.com>
2022-10-12 13:00:15 -0700

    Create OrderTwoCities.java

---
+++ src/unit1/OrderTwoCities.java
@@ -0,0 +1,27 @@
+package unit1;
+
+import java.util.Scanner;
+
+public class OrderTwoCities {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner input = new Scanner(System.in);
+		
+		// Prompt the user to enter two cities
+	    System.out.print("Enter the first city: ");
+	    String city1 = input.nextLine();
+	    System.out.print("Enter the second city: ");
+	    String city2 = input.nextLine();
+	    
+	    if (city1.compareTo(city2) < 0)
+	      System.out.println("The cities in alphabetical order are " +
+	         city1 + ", " + city2);
+	    else
+	      System.out.println("The cities in alphabetical order are " +
+	          city2 + ", " + city1);
+	    
+	    input.close();
+	}
+
+}
