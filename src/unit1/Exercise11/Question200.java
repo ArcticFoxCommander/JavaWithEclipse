2abf5f75b0d76447fa867b705d21fd1069ff8ff1
ArcticFoxCommander <60055478+ArticFoxCommander@users.noreply.github.com>
2022-10-14 13:43:54 -0700

    Update Question200.java

--- src/unit1/Exercise11/Question200.java
+++ src/unit1/Exercise11/Question200.java
@@ -1,12 +1,64 @@
 package unit1.Exercise11;
 
+import java.util.Scanner;
+
 public class Question200 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		
+		 Scanner in = new Scanner(System.in);
+         
+         System.out.print("Enter two characters: ");
+         String status = in.next();
+         
+         char major = Character.toUpperCase(status.charAt(0));
+         char year = status.charAt(1);
+         
+         String courseName = "";
+         String yearName = "";
+         
+         if (major == 'M' || major == 'I' || major == 'C')
+         {
+             switch(major)
+             {
+                 case 'M':
+                     courseName = "Mathematics";
+                     break;
+                 case 'C':
+                     courseName = "Computer Science";
+                     break;
+                 case 'I':
+                     courseName = "Information Technology";
+                     break;
+                 default:
+                     break;
+             }
+             
+             switch(year)
+             {
+                 case '1':
+                     yearName = "Freshman";
+                     break;
+                 case '2':
+                     yearName = "Sophmore";
+                     break;
+                 case '3':
+                     yearName = "Junior";
+                     break;
+                 case '4':
+                     yearName = "Senior";
+                     break;
+                 default:
+                     break;
+             }
+             System.out.printf("%s %s%n", courseName, yearName);
+         }
+         else{
+             System.out.printf("Invalid input.%n");
+             
+             in.close();
 		
 	}
-
+  }
 }
