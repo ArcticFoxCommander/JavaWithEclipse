076d066d7f0297a32bef183e51d83f7bbc6f932d
ArcticFoxCommander <60055478+ArticFoxCommander@users.noreply.github.com>
2022-10-17 09:41:20 -0700

    Create Question3.java

---
+++ src/unit2/Exercise1/Question3.java
@@ -0,0 +1,13 @@
+package unit2.Exercise1;
+
+public class Question3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i = 1;
+		while (i < 10)
+			if (i % 2 == 0)
+				System.out.println(i);
+	}
+
+}
