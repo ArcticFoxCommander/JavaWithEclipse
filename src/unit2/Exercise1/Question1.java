ed1a3274c64ee556ade711066b51a333ec5793e8
ArcticFoxCommander <60055478+ArticFoxCommander@users.noreply.github.com>
2022-10-17 09:40:54 -0700

    Create Question1.java

---
+++ src/unit2/Exercise1/Question1.java
@@ -0,0 +1,17 @@
+package unit2.Exercise1;
+
+public class Question1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int count = 0;
+		while (count < 100) {
+			// Point A = True
+			System.out.println("Welcome to Java");
+			count ++;
+			// Point B = True/False
+		}
+		// Point C = False
+	}
+
+}
