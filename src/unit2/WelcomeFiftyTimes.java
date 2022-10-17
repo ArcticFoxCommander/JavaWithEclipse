2dd9350ece11f2d1a7160eb961665a02204fc75d
ArcticFoxCommander <60055478+ArticFoxCommander@users.noreply.github.com>
2022-10-17 09:40:42 -0700

    Create WelcomeFiftyTimes.java

---
+++ src/unit2/WelcomeFiftyTimes.java
@@ -0,0 +1,16 @@
+package unit2;
+
+public class WelcomeFiftyTimes {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int i = 1;
+		while (i < 50) {
+			System.out.println("Welcome to Java");
+			++i;
+		}
+		
+	}
+
+}
