84091d4301e4db8b5af6fc5b8d6833822eed7b3c
ArcticFoxCommander <60055478+ArticFoxCommander@users.noreply.github.com>
2022-10-17 10:01:18 -0700

    Create LittieRepeated.java
    Repeat @Littie6amer 50 times :)

---
+++ src/unit2/LittieRepeated.java
@@ -0,0 +1,14 @@
+package unit2;
+
+public class LittieRepeated {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int i = 1;
+		while (i < 50) {
+			System.out.println("L     I     T     T     I     E");
+			++i;
+		}
+	}
+}
