c48e6e0157a8652fccc08cff4d1518702e817282
ArcticFoxCommander <60055478+ArticFoxCommander@users.noreply.github.com>
2022-10-17 09:42:14 -0700

    Create Question5.java

---
+++ src/unit2/Exercise1/Question5.java
@@ -0,0 +1,15 @@
+package unit2.Exercise1;
+
+public class Question5 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x = 80000000;
+		
+		while (x > 0)
+			x++;
+		
+		System.out.println("x is " + x);
+	}
+
+}
