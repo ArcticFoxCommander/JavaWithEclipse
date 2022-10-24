66ca08c92812657df06ca552136516a94959233f
ArcticFoxCommander <60055478+ArticFoxCommander@users.noreply.github.com>
2022-10-24 10:19:01 -0700

    Create LittieRepeated_2.java

---
+++ src/unit2/LittieRepeated_2.java
@@ -0,0 +1,14 @@
+package unit2;
+
+public class LittieRepeated_2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int i = 1;
+		while (i < 5000) {
+			System.out.println("LITTIE");
+			++i;
+		}
+	}
+}
