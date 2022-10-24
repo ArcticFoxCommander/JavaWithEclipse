0f157ccf5fdf73016dc5a39fe2a2e9dcb68b2a14
ArcticFoxCommander <60055478+ArticFoxCommander@users.noreply.github.com>
2022-10-24 09:46:38 -0700

    Create Question7.java

---
+++ src/unit2/Exercise2/Question7.java
@@ -0,0 +1,16 @@
+package unit2.Exercise2;
+
+public class Question7 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int counter = 1;
+		
+		do
+		{
+			System.out.println(counter);
+			counter +=1;
+		} while (counter <= 15);
+	}
+
+}
