39b0a8c381228f5e130d17b57f3017d4306b7556
ArcticFoxCommander <60055478+ArticFoxCommander@users.noreply.github.com>
2022-10-24 09:30:13 -0700

    Create PinAndPasswordChecker.java
    Littie 2979

---
+++ src/unit2/PinAndPasswordChecker.java
@@ -0,0 +1,37 @@
+package unit2;
+
+import java.util.Scanner;
+
+public class PinAndPasswordChecker {
+
+	public static void main(String[] args) {
+		// Create a Scanner
+		Scanner input = new Scanner(System.in);
+		int pin = 0;
+		// Keep reading data until the input is 0
+		do {
+			// Read the next data
+			System.out.print(
+					"Enter your pin: ");
+			pin = input.nextInt();
+		} while (pin != 2979);
+
+		System.out.println("The pin is correct");
+
+		System.out.print(
+				"Enter your password: ");
+		String pw = input.next();
+
+		switch (pw) {
+
+		case "Littie":
+			System.out.println("The password is correct");
+			break;
+		default:
+			System.out.println("Incorrect. Try again");
+			
+			input.close();
+		}
+	}
+}
+
