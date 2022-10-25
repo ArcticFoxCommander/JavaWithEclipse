0179599e0c5c3ad07a8c72b0d99767ff1539d45d
ArcticFoxCommander <60055478+ArticFoxCommander@users.noreply.github.com>
2022-10-25 11:23:35 -0700

    Create LittieSaidThisCodeWasFun.java
    @Littie6amer said it was a fun code.

---
+++ src/unit2/LittieSaidThisCodeWasFun.java
@@ -0,0 +1,13 @@
+package unit2;
+
+public class LittieSaidThisCodeWasFun {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i = 2;
+		while (i!=0) {
+		  System.out.println(i*=i);
+		}
+	}
+
+}
