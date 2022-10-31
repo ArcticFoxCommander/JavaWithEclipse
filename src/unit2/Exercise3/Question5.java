b0b52c1278911634d92482ed9bb371945c4baa44
ArcticFoxCommander <60055478+ArticFoxCommander@users.noreply.github.com>
2022-10-31 09:09:38 -0700

    Create Question5.java

---
+++ src/unit2/Exercise3/Question5.java
@@ -0,0 +1,18 @@
+package unit2.Exercise3;
+
+public class Question5 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for (int counter = 1; counter <= 100; counter++) {
+			System.out.println(counter);
+		}
+		
+		// You cannot use the variable from the loop but you can create a new variable with the same name.
+		
+		int counter = 8;
+		System.out.println(counter);
+		
+	}
+
+}
