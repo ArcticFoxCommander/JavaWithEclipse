24756182b62daacfb41c4a3700bfa6207d6543dd
ArcticFoxCommander <60055478+ArticFoxCommander@users.noreply.github.com>
2022-10-31 08:51:58 -0700

    Create Question2.java

---
+++ src/unit2/Exercise3/Question2.java
@@ -0,0 +1,11 @@
+package unit2.Exercise3;
+
+public class Question2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for (int counter = 1; counter <= 100; counter++) {
+			System.out.println(counter);
+		}
+	}
+}
