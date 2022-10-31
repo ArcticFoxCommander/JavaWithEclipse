0f420d967c37ad3c55ef614f5f990d10f9142ec5
ArcticFoxCommander <60055478+ArticFoxCommander@users.noreply.github.com>
2022-10-31 09:04:03 -0700

    Create Question3.java

---
+++ src/unit2/Exercise3/Question3.java
@@ -0,0 +1,23 @@
+package unit2.Exercise3;
+
+import java.util.Scanner;
+
+public class Question3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner input = new Scanner(System.in);
+		
+		int number, sum = 0, count;
+		
+		for (count = 0; count < 5; count++) {
+			System.out.print("Input number: ");
+			number = input.nextInt();
+			sum += number;
+		}
+		
+		System.out.println("Sum is " + sum);
+		System.out.println("Count is " + count);
+	}
+
+}
