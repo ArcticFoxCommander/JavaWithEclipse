0d7acb604b997d68aa3cdafd41d634830d537157
ArcticFoxCommander <60055478+ArticFoxCommander@users.noreply.github.com>
2022-10-31 08:56:43 -0700

    Create Question1.java

---
+++ src/unit2/Exercise3/Question1.java
@@ -0,0 +1,14 @@
+package unit2.Exercise3;
+
+public class Question1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int sum = 0;
+		for (int i = 0; i < 10; i++) {
+			sum += i;
+			System.out.println(sum);
+		}
+	}
+
+}
