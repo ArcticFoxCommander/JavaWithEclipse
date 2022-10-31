56417d8e3dc4b5457eb9be5891402d66c47fae9e
ArcticFoxCommander <60055478+ArticFoxCommander@users.noreply.github.com>
2022-10-31 09:27:08 -0700

    Create Question6.java

---
+++ src/unit2/Exercise3/Question6.java
@@ -0,0 +1,21 @@
+package unit2.Exercise3;
+
+public class Question6 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i = 0;
+//		while (i <= 1000) {
+//			System.out.println(i);
+//			i++;
+//		}
+		// Question 6 in while and do-while loop.
+		do {
+			System.out.println(i);
+			i++;
+		} while (i <= 1000);
+		
+		
+	}
+
+}
