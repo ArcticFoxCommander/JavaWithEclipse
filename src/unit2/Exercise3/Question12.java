c47136309c922346b6be710aecb72f9502c4cc66
ArcticFoxCommander <60055478+ArticFoxCommander@users.noreply.github.com>
2022-10-31 10:03:15 -0700

    Create Question12.java

---
+++ src/unit2/Exercise3/Question12.java
@@ -0,0 +1,14 @@
+package unit2.Exercise3;
+
+public class Question12 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i = 0;
+		do {
+		i += 3;
+		System.out.println(i);
+		} while (i <36);
+	}
+
+}
