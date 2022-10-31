fdb43080183582133fcac2cf6d0475f8759d8bf9
ArcticFoxCommander <60055478+ArticFoxCommander@users.noreply.github.com>
2022-10-31 09:14:13 -0700

    Update Question1.java

--- src/unit2/Exercise3/Question1.java
+++ src/unit2/Exercise3/Question1.java
@@ -5,7 +5,8 @@ public class Question1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int sum = 0;
-		for (int i = 0; i < 10; i++) {
+		for (int i = 0; i < 10; ++i) {
+			// i++ is also the same.
 			sum += i;
 			System.out.println(sum);
 		}
