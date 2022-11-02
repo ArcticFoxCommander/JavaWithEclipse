c5ed33bf31bf90df41dabaa830426d2f66ffac81
Fox <60055478+ArticFoxCommander@users.noreply.github.com>
2022-11-02 13:01:08 -0700

    Update Question9.java

--- src/unit2/Exercise3/Question9.java
+++ src/unit2/Exercise3/Question9.java
@@ -4,9 +4,10 @@ public class Question9 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int i = 1;
+		int sum = 0;
 		
-		for (int sum = 0; sum < 10000; sum++) {
+		for (int i = 1; sum < 10000; i++) {
+			sum = sum + i;
 			System.out.println(sum);
 		}
 	} 
