9bd42cd7aac2189ef650b330c53f999bc433ebde
ArcticFoxCommander <60055478+ArticFoxCommander@users.noreply.github.com>
2022-11-15 11:34:49 -0800

    Update Question2.java

--- src/unit2/Exercise5/Question2.java
+++ src/unit2/Exercise5/Question2.java
@@ -4,25 +4,25 @@ public class Question2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int sum = 0;
-		for (int i = 0; i < 4; i++) {
-			if (i % 3 == 0) continue;
-			sum += i;
-		}
-
-		/*
+		
+		//int sum = 0;
+		//for (int i = 0; i < 4; i++) {
+		//	if (i % 3 == 0) continue;
+		//	sum += i;
+		//}
+		
+		
 		 int i = 0, sum = 0;
 		    while (i < 4) {
-
-			if (i % 3 == 0) continue;
-			sum += i;
-
-			i++;
-
+		    	
+			if (i % 3 == 0) 
+				continue;
+				sum += i;
+				
 		}
-		 */
+		 
 
-		System.out.println(sum);
+		System.out.println("The sum is " + sum);
 	}
 
 }
