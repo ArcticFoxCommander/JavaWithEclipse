bbfc9822802108ed7fb66d494ece73cb9f87372b
ArcticFoxCommander <60055478+ArticFoxCommander@users.noreply.github.com>
2022-11-17 13:44:06 -0800

    Create Question5.java
    TBC

---
+++ src/unit2/Exercise5/Question5.java
@@ -0,0 +1,22 @@
+package unit2.Exercise5;
+
+import java.util.Scanner;
+
+public class Question5 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner input = new Scanner(System.in);
+		
+		System.out.print("Enter the first string: ");
+		String first = input.next();
+		System.out.print("Enter the second string: ");
+		String second = input.next();
+		
+		
+		
+		
+	}
+
+}
