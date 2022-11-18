0ca4b6c2528704fafc186f7690c392b593c18245
ArcticFoxCommander <60055478+ArticFoxCommander@users.noreply.github.com>
2022-11-18 13:59:14 -0800

    Update Question5.java
    Loops until the string has a common prefix.

--- src/unit2/Exercise5/Question5.java
+++ src/unit2/Exercise5/Question5.java
@@ -6,17 +6,30 @@ public class Question5 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+
 		Scanner input = new Scanner(System.in);
-		
-		System.out.print("Enter the first string: ");
-		String first = input.next();
-		System.out.print("Enter the second string: ");
-		String second = input.next();
-		
-		
-		
-		
+
+		int i = 0;
+
+		do {
+
+			System.out.print("Enter the first string: ");
+			String first = input.next();
+			System.out.print("Enter the second string: ");
+			String second = input.next();
+
+			if (first.indexOf(second) != -1) {
+				System.out.println("The common prefix is: " + second);
+				i++;
+			}
+			else {
+				System.out.println(first + " and " + second + " have no common prefix.\nTry again.");
+				continue;
+			}
+		} while (i == 0);
+
+		input.close();
 	}
 
 }
+
