32d56f16611833389181ccf382be5781ec5e09ce
ArcticFoxCommander <60055478+ArticFoxCommander@users.noreply.github.com>
2022-11-22 11:31:35 -0800

    Create Question8.java

---
+++ src/unit3/Exercise1/Question8.java
@@ -0,0 +1,34 @@
+package unit3.Exercise1;
+
+public class Question8 {
+
+	public static void main(String[] args) {
+
+	}
+	public static double salecommission(double amt, double rate) {
+		return amt * rate;
+	}
+
+	public static void cal(int mth, int year) {
+
+	}
+
+	public static double root(double x) {
+		return x * x;
+	}
+
+	public static boolean numb(int x) { 
+		return x % 2 == 0 ? true : false;
+	}
+
+	public static void printMessage(String message, int time) {
+	}
+
+	public static double monthlyPayment(double loanamt, double year, double air) {
+		return loanamt * year * air;
+	}
+
+	public static void upper(char lowerl) {
+	}
+}
+
