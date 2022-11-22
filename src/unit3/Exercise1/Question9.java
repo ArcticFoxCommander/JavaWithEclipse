4315553b763f4d057f7372de10eaf934425619b6
ArcticFoxCommander <60055478+ArticFoxCommander@users.noreply.github.com>
2022-11-22 11:34:11 -0800

    Update Question9.java

--- src/unit3/Exercise1/Question9.java
+++ src/unit3/Exercise1/Question9.java
@@ -10,12 +10,10 @@ public class Question9 {
 		n += m;
 		method2(3.4);
 	}
-
+	
 	public static int method2(double n) {
 		if (n > 0) return 1;
 		else if (n == 0) return 0;
 		else return -1;
 	}
 }
-
-
