ea4d4641f6e28bd1e7ce8fd89712e3e190a2e826
ArcticFoxCommander <60055478+ArticFoxCommander@users.noreply.github.com>
2022-11-22 11:31:40 -0800

    Create Question9.java

---
+++ src/unit3/Exercise1/Question9.java
@@ -0,0 +1,21 @@
+package unit3.Exercise1;
+
+public class Question9 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	}
+
+	public static void method1(int n, int m) {
+		n += m;
+		method2(3.4);
+	}
+
+	public static int method2(double n) {
+		if (n > 0) return 1;
+		else if (n == 0) return 0;
+		else return -1;
+	}
+}
+
+
