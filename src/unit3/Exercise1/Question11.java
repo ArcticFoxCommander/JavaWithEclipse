780a17e3ffce74fc0cd77d27e0cf24c12b3cc382
ArcticFoxCommander <60055478+ArticFoxCommander@users.noreply.github.com>
2022-11-24 13:54:56 -0800

    Update Question11.java

--- src/unit3/Exercise1/Question11.java
+++ src/unit3/Exercise1/Question11.java
@@ -1,34 +1,39 @@
 package unit3.Exercise1;
 
-import java.util.Scanner;
-
 public class Question11 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Scanner input = new Scanner(System.in);
+		java.util.Scanner input = new java.util.Scanner(System.in);
 		System.out.print("Input a permutation: ");
 		int a = input.nextInt();
 		System.out.println("Input a second permutation: ");
 		int b = input.nextInt();
-		
+
 		int p = 1;
+
+		for (int d = 1; d <= 9; d++) 
+			if (count(a, d) != count(b, d)) {
+				p = 0;
+				break;
+			}
+			
+			if (p == 1)
+				System.out.printf("%d is a permutation of %d\n" , a, b);
+			else
+				System.out.printf("%d is not a permutation of %d\n" , a, b);
+			input.close();
 		
-		for (int d = 1; d <= 9; d++) {
-		if (count(a, d) = count (b, a)) {
-			p = 0;
-			break;
-		}}
-		}
+	}
 
 	public static int count(int n, int d) {
 		int count = 0; 
-		while (n != 0) {
+		while (n > 0) {
 			if (n % 10 == d)
 				count++;
-			n = n / 10;
+			n /= 10;
 		}
 		return count;
 	}
-	
+
 }
