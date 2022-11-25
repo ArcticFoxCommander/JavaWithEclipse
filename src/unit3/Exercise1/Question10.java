73fa91daef72d0c41b301c27c2800af0246642b4
ArcticFoxCommander <60055478+ArticFoxCommander@users.noreply.github.com>
2022-11-25 09:37:08 -0800

    Create Question10.java

---
+++ src/unit3/Exercise1/Question10.java
@@ -0,0 +1,17 @@
+package unit3.Exercise1;
+
+public class Question10 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	}
+
+	public static double method(double i, double j) {
+		while (i < j) {
+			j--;
+		}
+
+		return j;
+	}
+
+}
