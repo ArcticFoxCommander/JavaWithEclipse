c50b5af5321495fefdee2126ff60ec12c93cb8e7
ArcticFoxCommander <60055478+ArticFoxCommander@users.noreply.github.com>
2022-11-28 09:20:00 -0800

    Create Question2.java

---
+++ src/unit3/Exercise2/Question2.java
@@ -0,0 +1,14 @@
+package unit3.Exercise2;
+
+public class Question2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		nPrintln("Welcome to Java", 5);
+	}
+	public static void nPrintln(String message, int n) {
+		int n1 = 1;
+		for (int i = 0; i < n1; i++)
+			System.out.println("Message");
+	}
+}
