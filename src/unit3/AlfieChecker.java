fd6127b2a8067680b3b17a4da25db97197024817
ArcticFoxCommander <60055478+ArticFoxCommander@users.noreply.github.com>
2022-12-01 14:36:00 -0800

    Update AlfieChecker.java
    idk why but added this

--- src/unit3/AlfieChecker.java
+++ src/unit3/AlfieChecker.java
@@ -25,6 +25,7 @@ public class AlfieChecker {
 
 		if (j == 0) {
 			System.out.println("Sorry! Alfie is home! Please run this checker tomorrow to see if you can relax!");
+			System.exit(0);
 		}
 		else
 			System.out.print("Whereabouts is Alfie? (0 = Outside | 1 = Kitchen | 2 = Living Room | 3 = Bathroom | 4 = Bedroom): ");
@@ -32,18 +33,23 @@ public class AlfieChecker {
 
 		if (k == 0) {
 			System.out.println("You can relax!");
+			System.exit(0);
 		}
 		else if (k == 1) {
 			System.out.println("You may relax! Just hope Alfie isn't helping in tonight's dinner!");
+			System.exit(0);
 		}
 		else if (k == 2) {
 			System.out.println("Sorry! Your discord calls may have a child in the background!");
+			System.exit(0);
 		}
 		else if (k == 3) {
 			System.out.println("You have limited time to relax! Unless Alfie is doing a #.");
+			System.exit(0);
 		}
 		else
 			System.out.println("You're basically screwed. Have fun getting some sleep or getting Alfie to sleep.");
+			System.exit(0);
 
 
 		input.close();			
