b7f05e00e27484866600a05a2d6bb10d35ee0d76
ArcticFoxCommander <60055478+ArticFoxCommander@users.noreply.github.com>
2022-12-01 14:44:33 -0800

    Create Question2.java

---
+++ src/unit3/Assignment1/Question2.java
@@ -0,0 +1,14 @@
+package unit3.Assignment1;
+
+public class Question2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	public static int numberOfDaysInAYear(int year) {
+		return 0;
+	}
+
+}
