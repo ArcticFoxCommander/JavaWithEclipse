88d47965352d0dd3c48b9f3b00b398a73c895296
ArcticFoxCommander <60055478+ArticFoxCommander@users.noreply.github.com>
2022-12-06 11:34:14 -0800

    Create Question7.java

---
+++ src/unit3/Exercise3/Question7.java
@@ -0,0 +1,25 @@
+package unit3.Exercise3;
+
+import java.util.Scanner;
+
+public class Question7 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner input = new Scanner(System.in);
+		
+		System.out.print("Input three numbers: ");
+		String password = input.next();
+		
+		checkPassword(password);
+	}
+	
+	public static boolean checkPassword(String pw) {
+		int a = 0;
+		int i = 0;
+		// for loop
+		pw.isLetter(a.charAt());
+		pw.length();
+	}
+	
+}
