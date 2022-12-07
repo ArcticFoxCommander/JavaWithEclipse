e0458a21b454d3d0fe2cebc424d0d024cabd0e0a
ArcticFoxCommander <60055478+ArticFoxCommander@users.noreply.github.com>
2022-12-07 13:02:19 -0800

    Update Question7.java

--- src/unit3/Exercise3/Question7.java
+++ src/unit3/Exercise3/Question7.java
@@ -7,19 +7,36 @@ public class Question7 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner input = new Scanner(System.in);
-		
-		System.out.print("Input three numbers: ");
-		String password = input.next();
-		
-		checkPassword(password);
+
+		System.out.print("Input a password: ");
+		String s = input.nextLine();
+
+		if (isValid(s)) {
+				System.out.println("Valid Password.");
+		}
+		else {
+			System.out.println("Invalid Password.");
+		}
 	}
-	
-	public static boolean checkPassword(String pw) {
-		int a = 0;
-		int i = 0;
-		// for loop
-		pw.isLetter(a.charAt());
-		pw.length();
+
+	public static boolean isValid(String s) {
+		int count = 0;
+		for (int i = 0; i < s.length(); i++) {
+			if (!Character.isLetter(s.charAt(i)) && !Character.isDigit(s.charAt(i)))
+				return false;
+
+			if (s.length() < 8)
+				return false;
+
+			if (Character.isDigit(s.charAt(i)))
+				count++;
+
+		}
+
+		if (count >= 2)
+			return true;
+		else
+			return false;
 	}
-	
+
 }
