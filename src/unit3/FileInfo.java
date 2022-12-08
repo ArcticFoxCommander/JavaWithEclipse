ac4458eac772d692f9ffeae1f7caeeeb9346d142
ArcticFoxCommander <60055478+ArticFoxCommander@users.noreply.github.com>
2022-12-08 14:42:56 -0800

    Update FileInfo.java

--- src/unit3/FileInfo.java
+++ src/unit3/FileInfo.java
@@ -1,10 +1,19 @@
 package unit3;
 
+import java.util.Scanner;
+
 public class FileInfo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		java.io.File file = new java.io.File("astolfoooooo.mp4");
+		Scanner input = new Scanner(System.in);
+		
+		System.out.print("Input the file name (INCLUDE FILE EXTENSION): ");
+		String i = input.next();
+		
+		input.close();
+		
+		java.io.File file = new java.io.File(i);
 		System.out.println("Does it exist? " + file.exists());
 		System.out.println("File Size: " + file.length() + " bytes");
 		System.out.println("Is it hidden? " + file.isHidden());
