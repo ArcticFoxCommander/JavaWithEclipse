16de215bfd646deb415b81324db60ae0ced5ee9b
ArcticFoxCommander <60055478+ArticFoxCommander@users.noreply.github.com>
2023-01-05 14:43:47 -0800

    Update Question15.java

--- src/unit4/Exercise1/Question15.java
+++ src/unit4/Exercise1/Question15.java
@@ -8,7 +8,7 @@ public class Question15 {
 
 		int[] array = new int[101];
 
-		System.out.print("Enter integers between 1 and 100: ");
+		System.out.print("Enter integers between 1 and 100 (REMEMBER TO PUT A 0 AT THE END): ");
 		int counter = input.nextInt();
 		while (counter != 0) {
 			array[counter]++;
@@ -20,6 +20,8 @@ public class Question15 {
 			else
 				if (array[index] == 1)
 					System.out.printf("%3d occurs %d time.\n", index, array[index]);
+		
+		input.close();
 	}
 }
 
