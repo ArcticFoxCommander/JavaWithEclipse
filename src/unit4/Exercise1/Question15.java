8ca41d9d735605304f87581d2ce956990ec4fcca
ArcticFoxCommander <60055478+ArticFoxCommander@users.noreply.github.com>
2023-01-05 14:32:53 -0800

    Update Question15.java

--- src/unit4/Exercise1/Question15.java
+++ src/unit4/Exercise1/Question15.java
@@ -5,15 +5,23 @@ public class Question15 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		java.util.Scanner input = new java.util.Scanner(System.in);
-		
-		int[] arr = new int[100];
-		
-		System.out.print("Enter the integers between 1 and 100: ");
-		for (int index = 0; index < arr.length; index++)
-		arr[index] = input.nextInt();
-		
-		for (int index = arr.length-1; index >= 0; index--)
-		System.out.println(arr[index]);
+
+		int[] array = new int[101];
+
+		System.out.print("Enter integers between 1 and 100: ");
+		int counter = input.nextInt();
+		while (counter != 0) {
+			array[counter]++;
+			counter = input.nextInt();
+
+			for(int index = 0; index <= array.length; index++)
+				if (array[index] > 1)
+					System.out.printf("%3d occurs %d times.\n", index, array[index]);
+				else
+					if (array[index] == 1)
+						System.out.printf("%3d occurs %d time.", index, array[index]);
+		}
 	}
 
 }
+
