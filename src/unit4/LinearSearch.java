d34477486be92638ffb44357b0bff987ed6e2a27
ArcticFoxCommander <60055478+ArticFoxCommander@users.noreply.github.com>
2023-01-11 13:29:09 -0800

    Create LinearSearch.java

---
+++ src/unit4/LinearSearch.java
@@ -0,0 +1,16 @@
+package unit4;
+
+public class LinearSearch {
+
+	public class LinearSxearch {
+		  /** The method for finding a key in the list */
+		  public static int linearSearch(int[] list, int key) {
+		    for (int i = 0; i < list.length; i++) {
+		      if (key == list[i])
+		        return i;
+		    }
+		    return -1;
+		  }
+		}
+
+}
