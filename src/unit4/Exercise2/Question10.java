05c6f4cd48d7545b85063d8f50eb041fea2fb980
ArcticFoxCommander <60055478+ArticFoxCommander@users.noreply.github.com>
2023-01-11 12:42:17 -0800

    Create Question10.java

---
+++ src/unit4/Exercise2/Question10.java
@@ -0,0 +1,48 @@
+package unit4.Exercise2;
+
+import java.util.Scanner;
+
+public class Question10 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner input = new Scanner(System.in);
+		double[] numbers = new double[10]; // Create an array of length ten
+
+
+
+		// Prompt the user to enter ten numbers
+		System.out.print("Enter ten numbers: ");
+		for (int i = 0; i < numbers.length; i++)
+			numbers[i] = input.nextDouble();
+
+
+
+		// Display the smallest value
+		System.out.println("The minimum number is " + 
+				numbers[indexOfSmallestElement(numbers)] + " index " +
+				indexOfSmallestElement(numbers));
+	}
+
+
+
+	/** Method indexOfSmallestElement returns the index of  
+	 *   the smallest element in an array of integers */
+	public static int indexOfSmallestElement(double[] array) {
+		if (array.length <= 1)
+			return 0;
+
+		double min = array[0];
+		int minimumIndex = 0;
+		for (int i = 1; i < array.length; i++) {
+			if (array[i] < min) {
+				min = array[i];
+				minimumIndex = i;
+			}
+		}
+		return minimumIndex;
+	}
+
+
+
+}
