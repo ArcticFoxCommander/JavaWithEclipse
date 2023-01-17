0d4933d2f97cc229dcfec0a34cbe5e6a452c288b
ArcticFoxCommander <60055478+ArticFoxCommander@users.noreply.github.com>
2023-01-17 11:34:14 -0800

    Create premiumfeatures.java

---
+++ src/premiumfeatures.java
@@ -0,0 +1,51 @@
+
+public class premiumfeatures {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+//				"features_v2": {
+//
+//
+//			        "Invite Tracking": [
+//			            "Access to __extra invite__ commands (/inviter, /invitedlist, ...)",
+//			            "Amount of blacklisted users for invites extended to __100__.",
+//			            "A list showing who was invited by a specific user or invite code.",
+//						"Ability to configure the __fake delay__ function."
+//			        ],
+//		        	"Join/Leave Messages": [
+//		   	 		"Configure the __vanity message type__ for join and leave messages.",
+//		        		"__Embeds__ for join and leave messages."
+//		    		],
+//			        "Leaderboard": [
+//			            "Ability to __export__ both the invites and messages leaderboard as a __.csv__ file.",
+//			            "Ability to __pin leaderboards__ in a channel.",
+//			            "Ability to see the leaderboard for a __specific role__."
+//			        ],
+//			        "Giveaways": [
+//			            "25% vote chance booster for giveaway __disabled__ in your server.",
+//			            "Up to __50 running giveaways.__",
+//			            "Up to __100 winners__ for your giveaways.",
+//			            "Option to __give roles to the winners__ of a giveaway.",
+//			            "Option to disable __giveaway entry DM__ message.",
+//						"Option to __DM the winners__ of a giveaway.",
+//			            "Ability to __edit giveaways__ on the dashboard.",
+//			            "__Unlimited__ blacklisted giveaway roles.", 
+//			            "__Unlimited__ giveaway bypass roles."
+//			        ],
+//			        "Messages": [
+//			            "__Unlimited__ reward roles for messages.",
+//			            "__Unlimited__ blacklisted channels for messages."
+//			        ],
+//			        "Administration": [
+//			            "__Unlimited__ auto roles.",
+//			            "Ability to access the __/massban__ command.",
+//			            "Ability to add and remove __fake invites__."
+//			        ]
+//
+//
+//			    }
+
+	}
+
+}
