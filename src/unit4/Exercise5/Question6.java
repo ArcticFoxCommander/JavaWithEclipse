a1faadc37b76df654089568d6ebce439f9a95547
ArcticFoxCommander <60055478+ArticFoxCommander@users.noreply.github.com>
2023-01-18 13:01:37 -0800

    Create Question6.java

---
+++ src/unit4/Exercise5/Question6.java
@@ -0,0 +1,46 @@
+package unit4.Exercise5;
+
+import java.util.Scanner;
+
+public class Question6 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner input = new Scanner(System.in);
+		int[] numbers = new int[10];
+		
+		System.out.print("Enter ten number: ");
+		for (int i = 0; i < numbers.length; i++)
+			numbers[i] = input.nextInt();
+		
+		int[] distinctNumbers = eliminateDuplicates(numbers);
+
+		System.out.print("The distinct numbers are:")
+		for (int e: distinctNumbers) {
+			if (e > 0)
+				System.out.print(" " + e);
+		}
+		System.out.println();
+	}
+
+	public static int[] eliminateDuplicates(int[] list) {
+		int[] distinctList = new int[list.length];
+		int i = 0;
+		for (int e: list) {
+			if (linearSearch(distinctList, e) == -1) {
+				distinctList[i] = e;
+				i++;
+			}
+		}
+		return distinctList;
+	}
+
+	public static int linearSearch(int[] array, int key) {
+		for (int i = 0; i < array.length; i++) {
+			if (key == array[i])
+				return i;
+		}
+		return -1;
+
+	}
+}
