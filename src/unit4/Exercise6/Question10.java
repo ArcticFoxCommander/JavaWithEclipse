c29b130465b8916dc1efecc25e71456bd6e4dd85
ArcticFoxCommander <60055478+ArticFoxCommander@users.noreply.github.com>
2023-01-19 14:39:24 -0800

    Create Question10.java

---
+++ src/unit4/Exercise6/Question10.java
@@ -0,0 +1,14 @@
+package unit4.Exercise6;
+
+public class Question10 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[][][] array = {{{1, 2}, {3, 4}}, {{5, 6},{7, 8}}};
+
+		System.out.println(array[0][0][0]);
+
+		System.out.println(array[1][1][1]);
+	}
+
+}
