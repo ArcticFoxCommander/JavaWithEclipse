42680de0019be6504d7b0495af4c6460abffae66
ArcticFoxCommander <60055478+ArticFoxCommander@users.noreply.github.com>
2023-01-19 14:40:20 -0800

    Create StupidLittieLanguage.java
    @Littie6amer speaks this language!

---
+++ src/StupidLittieLanguage.java
@@ -0,0 +1,14 @@
+
+public class StupidLittieLanguage {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String a = "a", b = "b", c = "c", d = "d", e = "e", f = "f", g = "g", 
+			   h = "h", i = "i", j = "j", k = "k", l = "l", m = "m", n = "n", o = "o", p = "p",
+			   q = "q", r = "r", s = "s", t = "t", u = "u", v = "v", w = "w", x = "x", y = "y", z = "z",
+			   one = "1", two = "2", three = "3", four = "4", five = "5", six = "6", seven = "7", eight = "8", nine = "9", zero = "0";
+		
+		System.out.print(two); 
+	}
+
+}
